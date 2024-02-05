a9d101889745863f63b8ae736eab94b97447c937
Darren Brain <darren.brain@gmail.com>
2024-02-05 17:07:19 -0500

    Fix illegal duplicate alias

--- models/Tag.js
+++ models/Tag.js
@@ -14,7 +14,7 @@ Tag.init(
       autoIncrement: true
     },
     tag_name: {
-      DataTypes:String
+      type: DataTypes.STRING
     }
   },
   {
--- models/index.js
+++ models/index.js
@@ -16,7 +16,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tag'
+  as: 'product_tags'
 
 })
 
@@ -26,7 +26,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   }, 
-  as: 'tag_product'
+  as: 'tag_products'
 })
 
 module.exports = {
