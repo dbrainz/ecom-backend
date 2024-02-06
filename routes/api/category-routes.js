aeceb5a15f761c1a99b7b4da01f2b3724c28dd82
Darren Brain <darren.brain@gmail.com>
2024-02-06 18:15:17 -0500

    Add 'add category', 'update category', and 'delete category' routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,12 +44,46 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    console.log(req.body, req.params.id);
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    if (!categoryData) {
+      res.status(404).json({message: 'No category found with this id!'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+    
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
